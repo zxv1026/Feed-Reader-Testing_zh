c7c17eb71748f3a09f19088bf90ee807a46a72f4
张晓钒 <zxv970209@outlook.com>
2018-08-22 23:45:26 +0800

    feature： 测试遍历 allFeeds 对象里面的所有的源，来保证有链接字段而且链接不是空的。

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -25,7 +25,12 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
-        
+        it('allFeeds链接有效且不为空',function () {
+            allFeeds.forEach(feed => {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+        } )
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
